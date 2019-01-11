4d3b1949dd50d1ab0d6aa4342ca6dd11cdf3a06e
itayanna <icage97@gmail.com>
2019-01-11 12:00:54 -0600

    typo change

--- Exercise5.js
+++ Exercise5.js
@@ -2,4 +2,4 @@
 var LOANAMET=parseInt(prompt("Enter your outstanding loan amount"));
 var NUMMONTHS=parseInt(prompt('How many months do you want it paid?'));
 var payments= Math.round(LOANAMET/NUMMONTHS);
-console.log("To pay of a loan of "+LOANAMET+ " it will take "+NUMMONTHS +" months");
+console.log("To pay off a loan of "+LOANAMET+ " in "+NUMMONTHS +" months, you will need to pay "+payments+" dollars every month");
