7eda83098360eebe7b7a7e955779dc85735039e6
itayanna <icage97@gmail.com>
2019-01-11 11:44:29 -0600

    forgot comment on num 5 i fixed it

--- Exercise5.js
+++ Exercise5.js
@@ -1,4 +1,4 @@
-//
+// tells how much you have to pay a month for the loan
 var LOANAMET=parseInt(prompt("Enter your outstanding loan amount"));
 var NUMMONTHS=parseInt(prompt('How many months do you want it paid?'));
 var payments= Math.round(LOANAMET/NUMMONTHS);
