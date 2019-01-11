e656f009e44f0226db889883e1f0d07d70a29601
itayanna <icage97@gmail.com>
2019-01-11 13:00:14 -0600

    bonus added

--- Exercise4.js
+++ Exercise4.js
@@ -1,4 +1,9 @@
 // converts temp into celcius
-var FTEMP=parseInt(prompt("what is the Fahrenheit Temp?"));
+
+do {
+    var FTEMP=parseInt(prompt("what is the Fahrenheit Temp?"));
+}while (isNaN(FTEMP));
 var CTEMP =parseInt(Math.round((FTEMP - 32) * 5 / 9));
 console.log(FTEMP+' Fahrenheit = '+CTEMP+" in Celcius");
+
+
--- Exercise5.js
+++ Exercise5.js
@@ -1,5 +1,10 @@
 // tells how much you have to pay a month for the loan
-var LOANAMET=parseInt(prompt("Enter your outstanding loan amount"));
-var NUMMONTHS=parseInt(prompt('How many months do you want it paid?'));
+do {
+    var LOANAMET=parseInt(prompt("Enter your outstanding loan amount"));
+}while (isNaN(LOANAMET));
+do {
+    var NUMMONTHS=parseInt(prompt('How many months do you want it paid?'));
+}while (isNaN(NUMMONTHS));
+
 var payments= Math.round(LOANAMET/NUMMONTHS);
 console.log("To pay off a loan of "+LOANAMET+ " in "+NUMMONTHS +" months, you will need to pay "+payments+" dollars every month");
